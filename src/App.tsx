ab34f003b3e26cc4cdffe8558171d9be0ba148a3
Camilo Cantoñi <juancamilocantoni2@gmail.com>
2025-12-04 18:08:23 -0500

    Subiendo el proyecto

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch("https://api.example.com/data");
+
+      if (!response.ok) {
+        throw new Error("Error en la solicitud de datos");
+      }
+
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (err) {
+      setError(err as string);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Cargando...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return;
+  <div>{JSON.stringify(data)}</div>;
+}
+
+export default App;
